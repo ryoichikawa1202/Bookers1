d4240a72df507c0b19ae7903b44221c48185a739
RyoIchikawa <95997394+ryoichikawa1202@users.noreply.github.com>
2022-04-06 09:27:04 +0000

    [add]editページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,6 +15,16 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
+  def destroy
   end
 
   private
